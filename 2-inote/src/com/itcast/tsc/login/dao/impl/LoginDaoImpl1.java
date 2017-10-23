b3b6b777f3e0ff15d0c5473cb234147c1e971718
zhangfengye <1050677051@qq.com>
2017-10-23 18:04:53 +0800

    this is the project of cloud notes

---
+++ 2-inote/src/com/itcast/tsc/login/dao/impl/LoginDaoImpl1.java
@@ -0,0 +1,13 @@
+package com.itcast.tsc.login.dao.impl;
+
+import com.itcast.tsc.login.dao.LoginDao;
+
+public class LoginDaoImpl1 implements LoginDao{
+
+	@Override
+	public boolean getLoginInfo(String userName, String password) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ 2-inote/src/com/itcast/tsc/login/service/LoginService.java
@@ -0,0 +1,13 @@
+package com.itcast.tsc.login.service;
+
+public interface LoginService {
+
+	/**
+	 * 登录
+	 * @param userName
+	 * @param password
+	 */
+	public boolean login(String userName,String password)throws Exception;
+	
+
+}
